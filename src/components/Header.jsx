fed48110ae0a99f58969edb9427fed4ebf5e4e12
AdalZanabria <adal.zanabria@gmail.com>
2022-03-27 20:29:33 -0600

    Uso de state para reflejar la cantidad de productos en el carrito del header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "@styles/Header.scss";
 import Menu from "@components/MenuDesktop";
+import AppContext from "@context/AppContext";
 import menu from "@icons/icon_menu.svg";
 import logo from "@logos/logo_yard_sale.svg";
 import shoppingCart from "@icons/icon_shopping_cart.svg";
 
 const Header = () => {
     const [toggle, setToggle] = useState(false);
+    const { state } = useContext(AppContext);
+
     const handleToggle = () => {
         setToggle(!toggle);
     };
@@ -43,7 +46,9 @@ const Header = () => {
                     </li>
                     <li className="navbar-shopping-cart">
                         <img src={shoppingCart} alt="shopping cart" />
-                        <div>2</div>
+                        {state.cart.length > 0 ? (
+                            <div>{state.cart.length}</div>
+                        ) : null}
                     </li>
                 </ul>
             </div>
