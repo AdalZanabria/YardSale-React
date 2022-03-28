fb04e9c8b3a1dd899ae2a0bf88abc2f1352a66da
AdalZanabria <adal.zanabria@gmail.com>
2022-03-27 19:49:07 -0600

    Agregado useRef a la page de Login para capturar los inputs del formulario.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,44 +1,56 @@
-import React from "react";
-import "../styles/Login.scss";
+import React, { useRef } from "react";
+import "@styles/Login.scss";
+import logo from "@logos/logo_yard_sale.svg";
 
 const Login = () => {
+    const form = useRef(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(form.current);
+        const data = {
+            username: formData.get("email"),
+            password: formData.get("password"),
+        };
+        console.log(data);
+    };
+
     return (
         <div className="login">
-            <div className="form-container">
-                <img
-                    src="./logos/logo_yard_sale.svg"
-                    alt="logo"
-                    className="logo"
-                />
-                <h1 className="title">Create a new password</h1>
-                <p className="subtitle">
-                    Enter a new password for your account
-                </p>
-                <form action="/" className="form">
-                    <label for="password" className="label">
+            <div className="login-container">
+                <img src={logo} alt="logo" className="logo" />
+
+                <form action="/" className="form" ref={form}>
+                    <label htmlFor="email" className="label">
+                        Email address
+                    </label>
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="correo@ejemplo.com"
+                        className="input input-email"
+                    />
+                    <label htmlFor="password" className="label">
                         Password
                     </label>
                     <input
                         type="password"
-                        id="password"
+                        name="password"
                         placeholder="*********"
                         className="input input-password"
                     />
-                    <label for="new-password" className="label">
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        id="new-password"
-                        placeholder="*********"
-                        className="input input-password"
-                    />
-                    <input
-                        type="submit"
-                        value="Confirm"
+                    <button
                         className="primary-button login-button"
-                    />
+                        onClick={handleSubmit}
+                    >
+                        Log in
+                    </button>
+                    <a href="/">Forgot my password</a>
                 </form>
+
+                <button className="secondary-button signup-button">
+                    Sign up
+                </button>
             </div>
         </div>
     );
